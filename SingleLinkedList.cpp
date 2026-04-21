8f042164bc5d6354988f033a3f8e1c1e51daa776
nanayyayaa <jiejueejuju@gmail.com>
2026-04-21 11:07:14 +0700

    commit 2

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -13,4 +13,19 @@ class LinkedList()
 {
     Node *START;
 
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
 
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+ 
