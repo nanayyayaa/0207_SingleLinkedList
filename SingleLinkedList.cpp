31c372dd38eaa39825033416748dbccc0fa8b48e
nanayyayaa <jiejueejuju@gmail.com>
2026-04-21 11:27:43 +0700

    commit 7

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -88,4 +88,18 @@ public:
         if (current == START)
             START = START->next
         else
- 
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData di dalam list adalah:\n";
