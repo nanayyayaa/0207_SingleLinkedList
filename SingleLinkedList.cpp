cbd4b3e083ffbed28ce36b077ce4b3c7e33e66bb
nanayyayaa <jiejueejuju@gmail.com>
2026-04-21 11:11:27 +0700

    commit 4

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -43,3 +43,18 @@ public:
 
         Node *previos = START;
         Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previos = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previos->next = nodeBaru;
+    }
